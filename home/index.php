750b35719a7b21cc9ed49dd3d433214f851ed000
Altamir Benkenstein <altamir.benkenstein@outlook.com>
2014-12-19 04:53:14 -0200

    Smarty testado e funcionando

--- home/index.php
+++ home/index.php
@@ -3,7 +3,7 @@ include_once '../configs/SmartyPortal.php';
 
 $sm = new SmartyPortal();
 
-$sm->assign("titulo", "Hello World!");
+$sm->assign("titulo", "Teste Smarty!");
 $sm->assign("teste", "Testando se smarty foi configurado corretamene!");
-
+$sm->debugar();
 $sm->display("teste.tpl");
