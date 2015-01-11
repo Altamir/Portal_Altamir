18a830b8ea7237ddeddb4a3a7ca29b2482a5c007
Altamir Benkenstein <altamir.benkenstein@outlook.com>
2015-01-11 03:41:40 -0200

    Adicionado informacoes sobre o projeto

--- home/sobre.php
+++ home/sobre.php
@@ -4,7 +4,17 @@ include_once '../configs/SmartyPortal.php';
 $sm = new SmartyPortal();
 
 $sm->assign("titulo", "Sobre");
-$sm->assign("teste", "aasdasdasdasd");
+$sm->assign("p1", "Projeto criado com o intuito de praticar conhecimentos"
+        . " adquirido nas aulas de Desenvolvimento Web e em curso de FRONT-END."
+        . " Inicialmente o portal conterá material produzido nas disciplinas do"
+        . " curso de Análise de Sistemas, dividido por disciplinas.");
+$sm->assign("p2","Na construção do site será utilizado no Back-end PHP e MySql."
+        . " Será utilizado Smarty Template na camada de apresentação."
+        . " No Front-End será utilizado Bootstrap para estilização. "
+        . "Após o site estiver com algumas funcionalidades prontas será "
+        . "desenvolvido uma versão mobile utilizando jQuery Mobile framework. ");
+$sm->assign("p3", "Assim que surgirem mais informações sobre o desenvolvimento "
+        . "serão postadas no site. O código está disponível no meu gitHub. ");
 $sm->assign("pg_atual", "Sobre");
 $sm->assign("img_cons", "http://www.venuebit.com/images/Page-Under-Construction.png");
 $sm->display("sobre.tpl");
--- templates/sobre.tpl
+++ templates/sobre.tpl
@@ -1,15 +1,19 @@
 {include file='include/topo.tpl' } 
-<div class="container">
-    <div class="row">
-        <div class="col-xs-12">
-            <div class="alert alert-success"><h1>{$teste}</div>
-        </div>
-    </div>
+<div class="container">    
     <div class="row">
         <div class="col-xs-offset-2 ">
 
             <img  class="img-responsive" src="{$img_cons}" alt="Site em construção" /> 
 
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="jumbotron">
+                    <p>{$p1}</p>
+                    <p>{$p2}</p>
+                    <p>{$p3}</p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
