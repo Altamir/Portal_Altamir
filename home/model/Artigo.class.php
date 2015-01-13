6021811bd102f766369e7388e5646f2ab1ac80d7
Altamir Benkenstein <altamir.benkenstein@outlook.com>
2015-01-13 13:20:34 -0200

    Classe modelArtigo

---
+++ home/model/Artigo.class.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Description of Artigo
+ *
+ * @author Altamir
+ */
+class Artigo {
+    private $id;
+    private $titulo;
+    private $autor;
+    private $data;
+    private $assunto;
+    private $tag;
+    private $secoes;
+    
+    function __construct() {
+        
+    }
+    function getTitulo() {
+        return $this->titulo;
+    }
+
+    function getAutor() {
+        return $this->autor;
+    }
+
+    function getData() {
+        return $this->data;
+    }
+
+    function getAssunto() {
+        return $this->assunto;
+    }
+
+    function getTag() {
+        return $this->tag;
+    }
+
+    function getSecoes() {
+        return $this->secoes;
+    }
+
+    function setTitulo($titulo) {
+        $this->titulo = $titulo;
+    }
+
+    function setAutor($autor) {
+        $this->autor = $autor;
+    }
+
+    function setData($data) {
+        $this->data = date('Y/m/d', strtotime($data));
+    }
+
+    function setAssunto($assunto) {
+        $this->assunto = $assunto;
+    }
+
+    function setTag($tag) {
+        $this->tag = $tag;
+    }
+
+    function setSecoes($secoes) {
+        $this->secoes = $secoes;
+    }
+
+
+}
