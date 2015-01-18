5ac4bd1d8d96fca14f7ba78ec7d4a5d9f2a41819
Altamir Benkenstein <altamir.benkenstein@outlook.com>
2015-01-18 03:46:22 -0200

    marcação para dados estruturados do Google

--- home/index.php
+++ home/index.php
@@ -4,6 +4,22 @@ include_once './DAO/MateriaDAO.class.php';
 
 $sm = new SmartyPortal();
 
+$sm->assign("marcacaoGoogle",'<!-- Marcação JSON-LD gerada pelo Assistente de marcação para dados estruturados do Google. -->
+<script type="application/ld+json">
+{
+  "@context" : "http://schema.org",
+  "@type" : "Article",
+  "name" : "Desenvolvimento Web",
+  "author" : {
+    "@type" : "Person",
+    "name" : "Altamir Benkenstein"
+  },
+  "image" : "http://altamir.com.br/home/imgs/altamir.jpg",
+  "articleSection" : "Projeto criado com o intuito de praticar conhecimentos adquirido",
+  "articleBody" : "utilizado no Back-end PHP e MySql. Será utilizado Smarty Template na camada de apresentação. No Front-End será utilizado Bootstrap para estilização. Após o site estiver com algumas funcionalidades prontas será desenvolvido uma versão mobile utilizando jQuery Mobile framework.",
+  "url" : "https://github.com/Altamir/Portal_Altamir"
+}
+</script>');
 $sm->assign("titulo", "Pagina em Construção!");
 $sm->assign("pg_atual", "Home");
 $sm->assign("teste", "Pagina em construção!" );
--- home/sobre.php
+++ home/sobre.php
@@ -9,6 +9,23 @@ $sm->assign("smMaterias",$materiaDAO->getAll());
 
 $smSecao = SecaoDAO::getInstacia();
 
+$sm->assign("marcacaoGoogle",'<!-- Marcação JSON-LD gerada pelo Assistente de marcação para dados estruturados do Google. -->
+<script type="application/ld+json">
+{
+  "@context" : "http://schema.org",
+  "@type" : "Article",
+  "name" : "Desenvolvimento Web",
+  "author" : {
+    "@type" : "Person",
+    "name" : "Altamir Benkenstein"
+  },
+  "image" : "http://altamir.com.br/home/imgs/altamir.jpg",
+  "articleSection" : "Projeto criado com o intuito de praticar conhecimentos adquirido",
+  "articleBody" : "utilizado no Back-end PHP e MySql. Será utilizado Smarty Template na camada de apresentação. No Front-End será utilizado Bootstrap para estilização. Após o site estiver com algumas funcionalidades prontas será desenvolvido uma versão mobile utilizando jQuery Mobile framework.",
+  "url" : "https://github.com/Altamir/Portal_Altamir"
+}
+</script>');
+
 $sm->assign("secoes", $smSecao->getBy(1));
 $sm->assign("titulo", "Sobre o Projeto");
 $sm->assign("pg_atual", "Sobre");
