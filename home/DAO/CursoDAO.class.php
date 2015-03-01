e2813e0a7500f6615dc41d4ac8cd501b37e8c18c
Altamir Benkenstein <altamir.benkenstein@outlook.com>
2015-03-01 05:38:51 -0300

    CursoDAo

--- home/DAO/CursoDAO.class.php
+++ home/DAO/CursoDAO.class.php
@@ -9,9 +9,11 @@ include_once 'model/Curso.class.php';
  * @author Altamir
  */
 class CursoDAO {
+
     private static $instancia;
-    
+
     private function __construct() {
+        
     }
 
     public static function getInstacia() {
@@ -20,18 +22,31 @@ class CursoDAO {
         }
         return self::$instancia;
     }
-    
-     public function getById($id) {
+
+    public function getAll() {
+        $sql = "SELECT * FROM curso";
+        $cursos = BD::getInstacia()->query($sql)->fetchAll();
+        $colecao[] = array();
+        foreach ($cursos as $linha) {
+            $aux = new Curso($linha['titulo'], $linha['descricao'], $linha['entidade'], $linha['ano']);
+            $aux->setId($linha['id']);
+            $colecao[] = $aux;
+        }
+        return $colecao;
+    }
+
+    public function getById($id) {
         $sql = "SELECT * FROM curso WHERE id = :id";
         $obj = null;
         $st = BD::getInstacia()->prepare($sql);
         $st->bindParam(":id", $id);
         $st->execute();
-        
-         if( ($linha = $st->fetch()) !== FALSE){
-             $obj = new Curso($linha['titulo'], $linha['descricao'] ,$linha['entidade'] ,$linha['ano']);
-             $obj->setId($id);
-         }
-         return $obj;
-     }
+
+        if (($linha = $st->fetch()) !== FALSE) {
+            $obj = new Curso($linha['titulo'], $linha['descricao'], $linha['entidade'], $linha['ano']);
+            $obj->setId($id);
+        }
+        return $obj;
+    }
+
 }
